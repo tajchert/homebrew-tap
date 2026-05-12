6945ac5641444a4fd3b770d6a0da864594c1c896
Michal Tajchert <mtajchert@gmail.com>
2026-05-12 02:29:12 +0200

    Enable harq Linux binaries

--- Formula/harq.rb
+++ Formula/harq.rb
@@ -25,7 +25,23 @@ class Harq < Formula
   end
 
   on_linux do
-    disable! date: "2026-05-12", because: "prebuilt Linux archives are not yet available"
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/tajchert/harq/releases/download/v0.2.0/harq_0.2.0_linux_amd64.tar.gz"
+      sha256 "232cfb2aa447eb13cb1046faafc7e5619a29260664f908441a1f72e458b586b6"
+
+      define_method(:install) do
+        bin.install "harq"
+      end
+    end
+
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/tajchert/harq/releases/download/v0.2.0/harq_0.2.0_linux_arm64.tar.gz"
+      sha256 "6ac8fc31a17c019b4c6d49953f1bead32c7c95073a9a4b0fc6d2a9b6d17ed44d"
+
+      define_method(:install) do
+        bin.install "harq"
+      end
+    end
   end
 
   test do
