87137ecb67cb90ac0120d8730130fc5ac5927710
Michal Tajchert <mtajchert@gmail.com>
2026-09-09 10:13:56 +0200

    Update Smallibre to 0.4.0

--- Casks/smallibre.rb
+++ Casks/smallibre.rb
@@ -1,6 +1,6 @@
 cask "smallibre" do
-  version "0.3.5"
-  sha256 "1eafe657b6ad32fe3a87f6271b53f476dc5cdf1fffd059d653a982166fe77cae"
+  version "0.4.0"
+  sha256 "2c4165e66cdb419326be5d8ff71f38c2f4bb8379d8ec08115ab29c55883415d8"
 
   url "https://github.com/tajchert/smallibre/releases/download/v#{version}/Smallibre-#{version}-macos-arm64.zip"
   name "Smallibre"
@@ -8,7 +8,7 @@ cask "smallibre" do
   homepage "https://github.com/tajchert/smallibre"
 
   livecheck do
-    skip "Early-alpha releases are updated manually"
+    skip "Releases are updated manually"
   end
 
   depends_on arch: :arm64
@@ -17,7 +17,7 @@ cask "smallibre" do
   app "Smallibre.app"
 
   caveats <<~EOS
-    This early-alpha build is ad-hoc signed and is not notarized.
+    This build is ad-hoc signed and is not notarized.
     If macOS blocks the first launch, review Smallibre in System Settings >
     Privacy & Security and choose Open Anyway if you trust this release.
   EOS
